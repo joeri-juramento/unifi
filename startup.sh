6b5c508abfada17ce54cdeb9dd64a7a3b6a26e89
joeri-juramento <22143678+joeri-juramento@users.noreply.github.com>
2023-02-07 11:02:08 +0000

    Disabled deleting of backup files
    removed -d switch from rsync command so backup files are not deleted from bucket beyond unifi's serverside imposed limit.

--- startup.sh
+++ startup.sh
@@ -298,7 +298,7 @@ After=network-online.target
 Wants=network-online.target
 [Service]
 Type=oneshot
-ExecStart=/usr/bin/gsutil rsync -r -d /var/lib/unifi/backup gs://$bucket
+ExecStart=/usr/bin/gsutil rsync -r /var/lib/unifi/backup gs://$bucket
 _EOF
 
 	cat > /etc/systemd/system/unifi-backup.timer <<_EOF
